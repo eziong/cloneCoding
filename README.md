820afe37cb437c108f305bed06a55a4a7f81832d
eziong <bsjwoong@gmail.com>
2021-02-11 21:56:39 +0900

    -

---
+++ README.md
@@ -0,0 +1,1 @@
+Clone coding for practicing how to code
---
+++ whatsapp/client/src/components/Contacts.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useContacts } from "../contexts/ContactsProvider";
+
+export default function Contracts() {
+    const { contacts } = useContacts();
+
+    return (
+        <ListGroup variant="flush">
+            {contacts.map((contact) => {
+                return (
+                    <ListGroup.Item key={contact.id}>
+                        {contact.name}
+                    </ListGroup.Item>
+                );
+            })}
+        </ListGroup>
+    );
+}
