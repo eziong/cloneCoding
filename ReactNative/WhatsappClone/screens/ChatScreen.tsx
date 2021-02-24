f282c9b56995a52ce7e87b1859e3fb49d1621a42
eziong <bsjwoong@gmail.com>
2021-02-24 20:34:45 +0900

    ChatRoom List (FlatList)

---
+++ ReactNative/WhatsappClone/screens/ChatScreen.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import ChatListItem from '../components/ChatListItems'
+
+import ChatRooms from '../data/ChatRooms'
+import { FlatList } from 'react-native-gesture-handler';
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{width:'100%'}}
+        data={ChatRooms}
+        renderItem={({item})=>(
+          <ChatListItem chatRoom={item} />
+        )}
+        keyExtractor={(item)=>item.id}
+        />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ ReactNative/WhatsappClone/types.tsx
@@ -0,0 +1,37 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type MainTabParamList = {
+  Camera: undefined;
+  Chats: undefined;
+  Status: undefined;
+  Calls: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
+
+export type User = {
+  id:String;
+  name: String;
+  imageUri:String;
+}
+
+export type Message = {
+  id:String;
+  content: String;
+  createdAt:String;
+}
+
+export type ChatRoom = {
+  id : String;
+  users:[User];
+  lastMessage:Message;
+}
